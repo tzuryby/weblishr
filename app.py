1bfa1a3232d5fc9061e4599a776c2b1e4ff5c092
tzury bar yochay <tzury@regulus.(none)>
2008-11-20 11:11:18 +0200

    got something that works

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ urls = (
 
 class Admin(object):
     def GET(self, url):
-        print web.storage(header='foo', author='foo', pub_date='foo', content='admin')
+        return web.storage(header='foo', author='foo', pub_date='foo', content='admin')
         
     def POST(self):
         pass
@@ -20,27 +20,35 @@ class Ajax(object):
 class Page(object):
     def GET(self, url=''):
         p = load_page(url)
-        print render.base(None)#(render.page(p))
-        #print render.page(p)
+        return base_template(render.post(p, None))
         
+def base_template(page):
+    return render.base(
+        render.header(site_globals), 
+        render.footer(), 
+        page, 
+        site_globals)
+    
 def load_page(url):
     ''' will return the matched url or None '''
     if url == '':
         return load_frontier()
         
-    rows= list(web.select('objects', where = 'url=$url', vars=locals()))
+    rows= db.select('objects', where = 'url=$url', vars=locals()).list()
     return len(rows) and rows[0]
     
 def load_frontier():
     data = web.storage(title='foo', author='foo', pub_date='foo', content='home page')
     return data
-    
 
-web.config.db_parameters = dict(dbn='sqlite', db='db/weblishr.db')
 web.webapi.internalerror = web.debugerror
 web.webapi.notfound = lambda: "page not found"
+
+db = web.database(dbn='sqlite', db='db/weblishr.db')
+site_globals = web.storage(site_name='Weblishr', title='Weblishr: green publisher')
 
 render = web.template.render('templates/')
 
 if __name__== "__main__":
-    web.run(urls, globals(), web.reloader)
+    web.application(urls, globals()).run()
+    
