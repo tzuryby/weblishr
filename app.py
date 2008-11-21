371c8f24fdbccf750bd618f3942986a1586d6d4e
tzury bar yochay <tzury@regulus.(none)>
2008-11-22 00:44:07 +0200

    next step would be the edit

--- app.py
+++ app.py
@@ -2,22 +2,24 @@ from datetime import date, timedelta
 import web
 
 urls = (
-    '/(.*)', 'Page',
-    '/admin/(*)', 'Admin',
-    '/ajax/(*)', 'Ajax',
+    '/edit/(.*)', 'Editor',
+    '/(.*)', 'View',
 )
 
-class Admin(object):
+web.webapi.internalerror = web.debugerror
+notfound = web.webapi.notfound = lambda url: render.notfound(url)
+web.template.Template.globals['sorted'] = sorted
+db = web.database(dbn='sqlite', db='db/weblishr.db')
+site_globals = web.storage(site_name='Weblishr', title='Weblishr: green publisher')
+
+render = web.template.render('templates/')
+        
+class Editor(object):
     def GET(self, url):
-        pass #return web.storage(header='foo', author='foo', pub_date='foo', content='admin')
+        page =_get_object(url) or notfound(url)
+        return base_template(render.edit(page))
         
-    def POST(self):
-        pass
-        
-class Ajax(object):
-    pass
-        
-class Page(object):
+class View(object):
     def GET(self, url=''):
         return base_template(load_page(url))
         
@@ -28,37 +30,33 @@ def base_template(page):
         page, 
         site_globals)
     
-def load_post(url):
-    rows= db.select('objects', where = 'url=$url', vars=locals()).list()
-    p = len(rows) and rows[0]
-    if p:
-        return render.post(p, None)
-        
 def load_page(url):
     ''' will return the matched url or None '''
-    return url == '' and  load_frontier() \
+    page = url == '' and  load_frontier() \
         or load_post(url)
+    return page or notfound(url)
+
+def _get_object(url):
+    rows= db.select('objects', where = 'url=$url', vars=locals()).list()
+    o = len(rows) and rows[0]
+    return o
     
+def load_post(url):
+    post =_get_object(url)
+    if post:
+        return render.post(post)
     
 def load_frontier():
     # date and time of the first back day
     dt = (date.today() - timedelta(days= 7)).timetuple()
     since = str(dt[0]) + '-' + str(dt[1]) + '-' + str(dt[2])
     rows = db.select('objects', where = 'pub_date >= $since', vars=locals()).list()
-    args = web.storage()
-    sections = set((row.section for row in rows))
-    for section in sections:
-        args[section] = [row for row in rows if row.section == section]
-
+    sections = sorted(set((row.section for row in rows)))
+    args = web.storage(
+        ((section,[row for row in rows if row.section == section]) for section in sections)
+    )
     return render.home(web.storage(args))
 
-web.webapi.internalerror = web.debugerror
-web.webapi.notfound = lambda: "page not found"
-
-db = web.database(dbn='sqlite', db='db/weblishr.db')
-site_globals = web.storage(site_name='Weblishr', title='Weblishr: green publisher')
-
-render = web.template.render('templates/')
 
 if __name__== "__main__":
     web.application(urls, globals()).run()
