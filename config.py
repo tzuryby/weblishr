aa43b4b3ae70f9864511b8a014df9578f6f9a115
tzury bar yochay <tzury@regulus.(none)>
2008-11-22 18:53:48 +0200

    added WMEditor files

---
+++ config.py
@@ -0,0 +1,15 @@
+import web
+
+__all__ = ['site_globals']
+
+site_globals = web.storage(
+
+    site_name='Weblishr', 
+    
+    title='Weblishr: green publisher',
+    
+    author = 'Tzury Bar Yochay',
+    
+    posts_per_section = 7,
+    
+)
